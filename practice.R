19cafec4b6e3d5cb951fdf41698b58ff6787c403
cbourquin <camibourquin@gmail.com>
2014-07-03 09:59:24 -0700

    added title to plot

--- practice.R
+++ practice.R
@@ -3,6 +3,7 @@ library(ggplot2)
 x <- seq(from=-6, to=6, len=100)
 y <- sin(x)
 
-ggplot(as.data.frame(x=x, y=y), aes(x=x, y=y)) + 
+gg <-  ggplot(as.data.frame(x=x, y=y), aes(x=x, y=y)) + 
   geom_point() +
-  geom_line()
+  geom_line() +
+  ggtitle("sin(x)")
